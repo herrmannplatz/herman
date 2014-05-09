0a8b146eada0778c41f0aab612980a389a8f1929
herrmannplatz <rene@herrmannplatz.de>
2014-05-09 19:42:31 +0200

    updated update-ph-pages

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
 #
-# This script will update the gh-pages branch with any changes that have been made to the steps
-# and to the dependencies.
+# This script will update the gh-pages branch
 # 
 
-# Exit on error and print each command as it is run
-set -ex
+# Go to gh-pages
+git checkout gh-pages
 
-ROOT_DIR=`dirname $0`/..
-
-# Get into the correct directory
-cd $ROOT_DIR
-
-# Make sure we are on master
-git checkout -f master
-
-# Ensure that all the tool dependencies are there
-npm install
-
-# Move the snapshot step folders into the gh-pages branch
-git checkout -f gh-pages
-
-# Commit any changes
-git add --all 
-git commit -m"update gh pages" || true
-
-# Display an info message, including the last two commits
-echo gh-pages has been updated. See the log below.
-git log -2
+# bring gh-pages up to date
+git rebase master
 
 # Push changes to the gh-pages branch on GitHub
 git push origin gh-pages
+
+# Go Back to master
+git checkout master
